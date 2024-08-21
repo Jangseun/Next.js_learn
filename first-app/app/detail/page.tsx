3b01790e6070dde13645287f804e912a1ad701bf
Jangseun <164428037+Jangseun@users.noreply.github.com>
2024-08-21 20:51:57 +0900

    learn

--- first-app/app/detail/page.tsx
+++ first-app/app/detail/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="conntaiar">침대가 좋은 한 사람의 사이트</h1>
-      <div className="title">하핳ㅎ</div>
-    </div>
-  );
-}
+// export default function Home() {
+
+//   const value = "typescript 최고"
+
+//   return (
+//     <div>
+//       <div className= {styles.container}>
+//         <h1 className={styles.title}>침대가 좋은 한 사람의 사이트</h1>
+//         <Link href="/detail">안녕하세요</Link>
+//         <p>{value}</p><br />
+
+//       </div>
+//     </div>
+//   );
+// }
--- first-app/app/page.tsx
+++ first-app/app/page.tsx
@@ -2,10 +2,31 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const numList:number[] = [2,3,4,5,6,7,8,9];
+  const nList:number[] = [1,2,3,4,5,6,7,8,9];
+
+  //중요
+  [1,2,3,4].map((value, i) => {
+    value * value
+  });
+
   return (
-    <div>
-      <h1 className="conntaiar">침대가 좋은 한 사람의 사이트</h1>
-      <div className="title">하핳ㅎ</div>
-    </div>
+    <div className={styles.container}>
+      {numList.map((value, i) => {
+        return(
+          <div key={i}>
+            <h1>{value}단</h1>
+            {nList.map((n, j) => {
+              return(
+                <h2 key={i}>
+                  {value} * {n} = {value*n}
+                </h2>
+              ) 
+            })}
+            <br />
+          </div>
+        );
+      })}
+      </div>  
   );
 }
