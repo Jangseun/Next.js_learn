439ee3814645dbd4eff589a6725d09dd439b1789
JangSeung <0sunghee122@gmail.com>
2024-09-05 19:14:41 +0900

    .

--- shadcn2/src/app/page.tsx
+++ shadcn2/src/app/page.tsx
@@ -1,16 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { ChevronLeft } from "lucide-react";
-import { Calendar } from "@/components/ui/calendar"
-"use client" 
+import { ChevronLeft } from "lucide-react"; 
 
 
 export default function Home() {
   return (
     <div>
       <div className="flex justify-center items-center h-screen">
-        <Button onClick={() => {
-          console.log("Button clicked!");
-        }} variant={"outline"} size={"icon"}>
+        <Button variant={"outline"} size={"icon"}>
           <ChevronLeft />
         </Button>
       </div>
