c3c7e801a771ebfbd970ba83b851c9a7afbe5114
JangSeung <0sunghee122@gmail.com>
2024-09-06 19:59:23 +0900

    아니 근데 ㄹㅇ 심심했음

--- shadcn2/src/app/page.tsx
+++ shadcn2/src/app/page.tsx
@@ -10,19 +10,101 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function Home() {
   return (
     <div className="h-screen w-full">
-      <div className="bg-amber-400 w-full h-20">
+      <div>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>File</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>
+                New Window <MenubarShortcut>⌘N</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem disabled>New Incognito Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarSub>
+                <MenubarSubTrigger>Share</MenubarSubTrigger>
+                <MenubarSubContent>
+                  <MenubarItem>Email link</MenubarItem>
+                  <MenubarItem>Messages</MenubarItem>
+                  <MenubarItem>Notes</MenubarItem>
+                </MenubarSubContent>
+              </MenubarSub>
+              <MenubarSeparator />
+              <MenubarItem>
+                Print... <MenubarShortcut>⌘P</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>집이좋음</MenubarTrigger>
+            <MenubarContent>
+              <MenubarSub>
+                <MenubarSubTrigger>그렇죠?</MenubarSubTrigger>
+                <MenubarSubContent>
+                  <MenubarItem>당연</MenubarItem>
+                  <MenubarItem>ㅇ</MenubarItem>
+                  <MenubarItem>YES or YES</MenubarItem>
+                </MenubarSubContent>
+              </MenubarSub>
+              <MenubarItem>뭐적지</MenubarItem>
+              <MenubarItem>하핳ㅎ</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </div>
+
+      <div className="bg-amber-400 w-full h-40 flex items-center">
+        <Avatar className="w-24 h-24 mx-10">
+          <AvatarImage src="https://img.freepik.com/premium-vector/village-modern-beautiful-house-free-vector_1167724-225.jpg?semt=ais_hybrid" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="text-white font-bold w-full h-30 text-7xl">집이 최고야!</div>
       </div>
 
       <div>
-        <Textarea placeholder="Type your message here.">
+        <Textarea placeholder="하하 암거나 적어라" className="mt-5">
         </Textarea>
       </div>
 
+
       <div className="flex justify-center">
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3 w-60 h-60 border text-red-500 text-5xl font-bold flex items-center justify-center m-5">집</CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3 w-60 h-60 border text-orange-400 text-5xl font-bold flex items-center justify-center m-5">가</CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3 w-60 h-60 border text-yellow-500 text-5xl font-bold flex items-center justify-center m-5">고</CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3 w-60 h-60 border text-green-600 text-5xl font-bold flex items-center justify-center m-5">싶</CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3 w-60 h-60 border text-blue-500 text-5xl font-bold flex items-center justify-center m-5">당</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+
+      </div>
+            <div className="flex justify-center">
         <div className="flex justify-center">
         <Button variant={"outline"} size={"icon"}>
           <ChevronLeft />
@@ -47,23 +129,6 @@ export default function Home() {
           </Button>
         </div>
       </div>
-
-      <div className="flex justify-center">
-        <Carousel>
-          <CarouselContent>
-            <CarouselItem className="basis-1/3 w-40 h-40 border ">집</CarouselItem>
-            <CarouselItem className="basis-1/3 w-40 h-40 border">가</CarouselItem>
-            <CarouselItem className="basis-1/3 w-40 h-40 border">고</CarouselItem>
-            <CarouselItem className="basis-1/3 w-40 h-40 border">싶</CarouselItem>
-            <CarouselItem className="basis-1/3 w-40 h-40 border">당</CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-
-      </div>
-      
-
     </div>
   );
 }
